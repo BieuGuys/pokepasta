c38cafa6657667ba3017bec253ce64982c2e2740
BieuGuys <114687628+BieuGuys@users.noreply.github.com>
2023-01-01 12:30:48 -0300

    Add files via upload

---
+++ assets/preload/data/prevention/script.lua
@@ -0,0 +1,10 @@
+function onSongStart()
+    noteTweenX('oppo0', 0, -1000, 0.1, 'quartInOut')
+    noteTweenX('oppo1', 1, -1000, 0.1, 'quartInOut')
+    noteTweenX('oppo2', 2, -1000, 0.1, 'quartInOut')
+    noteTweenX('oppo3', 3, -1000, 0.1, 'quartInOut')
+    noteTweenX('play0', 4, 415, 0.1, 'quartInOut')
+    noteTweenX('play1', 5, 525, 0.1, 'quartInOut')
+    noteTweenX('play2', 6, 635, 0.1, 'quartInOut')
+    noteTweenX('play3', 7, 745, 0.1, 'quartInOut')
+end
